a6dd79ab3e76eae1e0195d044fb27a200d8b8b76
Felipe Almeida <felipe.almeida@skeelo.app>
2024-04-02 19:15:45 -0300

    Initial Commit

---
+++ StandardTemplate/StandardTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandardTemplateApp.swift
+//  StandardTemplate
+//
+//  Created by Felipe Almeida on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct StandardTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
